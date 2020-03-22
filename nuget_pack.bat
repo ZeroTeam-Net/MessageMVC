ff983348a87dbacd99f72e78267d01deef23a48a
Agebull <agebull@qq.com>
2020-03-22 18:06:39 +0800

    发布1.0.0.3-bate包到Nuget

--- nuget_pack.bat
+++ nuget_pack.bat
@@ -1,5 +1,8 @@
-cd C:\Projects\ZeroTeam\MessageMVC\src
+cd C:\Projects\ZeroTeam\MessageMVC
+cd nuget
+del *.nupkg
+cd ..
 dotnet pack ZeroTeam.MessageMVC.sln -c:"Release" -o:"C:\Projects\ZeroTeam\MessageMVC\nuget"
-cd C:\Projects\ZeroTeam\MessageMVC\nuget
+cd nuget
 del *.1.0.0.nupkg
 dotnet nuget push *.nupkg -k oy2gica5n5ejwx6dwbaqimhqo6m7hl5evcnn63oisjnwfy -s https://api.nuget.org/v3/index.json
